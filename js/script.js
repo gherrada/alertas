d3ececf6a85e5fea8653e56f4632673099f6db75
gherrada <gonzalo.herrada@gmail.com>
2023-10-25 17:09:26 -0300

    inicio del ejemplo

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+function freeSubscribeButton(){
+    swal({
+        "title": "Felicidades",
+        "text": "Te has suscrito gratis al producto de ejemplo ! (no hay producto)",
+        "icon": "success"
+    });
+}
+
+function proSubscribeButton(){
+    swal({
+        "title": "Bienvenido",
+        "text": "Te has suscrito a la versión pro del producto de ejemplo ! (no hay producto)",
+        "icon": "success"
+    });
+}
+
+function enterpriseSubscribeButton(){
+    swal({
+        "title": "Error",
+        "text": "El producto enterprise no esta disponible aun",
+        "icon": "error"
+    });
+}
